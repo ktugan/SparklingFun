ae95e93b8e54b90d26b444ee95b2cacdbcc68b4c
Kadir Tugan <kadir.tugan@sevenval.com>
2015-02-10 14:08:05 +0100

    reverting temporary changes in main

--- src/main/scala/sparkapps/Main.scala
+++ src/main/scala/sparkapps/Main.scala
@@ -6,16 +6,7 @@ import breeze.numerics._
 object Main {
 
   def main(args: Array[String])= {
-    //val algorithms = Array(SimpleCounter, MorrisCounter, RevisedMorrisCounter)
-    //TwitterStreaming.startTwitterStreamAlgorithm(algorithms)
-
-
-    for(i<-0 to 10000){
-      RevisedMorrisCounter.probably_increment(null)
-      MorrisCounter.probably_increment(null)
-    }
-
-    MorrisCounter.probably_increment(null)
-
+    val algorithms = Array(SimpleCounter, MorrisCounter, RevisedMorrisCounter)
+    TwitterStreaming.startTwitterStreamAlgorithm(algorithms)
   }
 }
